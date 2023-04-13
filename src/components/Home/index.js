cae967cfc714791b06fb9bccc556d21de2f011ff
hppanpaliya <hppanpaliya@gmail.com>
2023-04-13 08:48:55 -0400

    new Home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,131 +1,142 @@
-// home/index.js
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Typography, Box, Container, Grid } from '@mui/material';
+import React, { useRef } from "react";
+import { Typography, Box, Container, Grid, Card, CardContent } from "@mui/material";
+import { motion, useAnimation } from "framer-motion";
+import slideImages from "./slideImages.json";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
+  const [currentSlide, setCurrentSlide] = useState(1);
+  const slideRefs = useRef([]);
+
+  const handleScroll = () => {
+    slideRefs.current.forEach((ref, index) => {
+      const slideHeight = ref.getBoundingClientRect().height;
+      const slideTop = ref.getBoundingClientRect().top;
+      const slideBottom = slideTop + slideHeight;
+
+      if (slideTop < window.innerHeight * 0.7 && slideBottom > window.innerHeight * 0.7) {
+        const tag = ref.dataset.tag;
+        const slideIndex = slideImages.findIndex((slide) => slide.tag === tag);
+        console.log("scrolling", index);
+        setCurrentSlide(index + 1);
+      }
+    });
+  };
+
+  const scrollToSlide = (index) => {
+    slideRefs.current[index].scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    slideRefs.current = slideRefs.current.slice(0, slideImages.length);
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <Box>
-      <Container maxWidth="md">
+      <Box>
         <Typography variant="h4" align="center" gutterBottom>
           Welcome to our Mental Health Support Web App
         </Typography>
         <Typography variant="h5" align="center" gutterBottom>
           Discover our features
         </Typography>
-
-        <Grid container spacing={4}>
-          <Grid item xs={12}>
-            <Typography variant="h6">App Overview</Typography>
-            <Typography variant="body1">
-              Welcome to our Mental Health Support Web App! Our app provides a range of resources and tools to support mental health and well-being.
+{/* 
+<Box sx={{ display: 'flex', justifyContent: 'center', marginBottom: 4 }}>
+  {slideImages.map((slide, index) => (
+    <Box key={index} sx={{ marginX: 2 }}>
+{slide.items && (
+  <ul>
+    {slide.items.slice(1).map((item, idx) => (
+      <li key={idx}>
+        {slide.links && slide.links[idx] ? (
+          <Link to={slide.links[idx]}>
+            <Typography variant="body1" align="center">
+              {item}
             </Typography>
-          </Grid>
+          </Link>
+        ) : (
+          <Typography variant="body1" align="center">
+            {item}
+          </Typography>
+        )}
+      </li>
+    ))}
+  </ul>
+)}
 
-          <Grid item xs={12}>
-            <Typography variant="h6">Games</Typography>
-            <ul>
-              <li>
-                <Typography variant="body1">
-                  Memory Match: Test your memory skills.
-                </Typography>
-              </li>
-              <li>
-                <Typography variant="body1">
-                  DrawingApp: Unleash your creativity.
-                </Typography>
-              </li>
-            </ul>
-          </Grid>
 
-          <Grid item xs={12}>
-            <Typography variant="h6">Social</Typography>
-            <ul>
-              <li>
-                <Typography variant="body1">
-                  Support Groups: Join virtual support groups.
-                </Typography>
-              </li>
-              <li>
-                <Typography variant="body1">
-                  Chat: Connect with peers.
-                </Typography>
-              </li>
-            </ul>
-          </Grid>
+      
+    </Box>
+  ))}
+        </Box>
+         */}
+<br />
 
-          <Grid item xs={12}>
-            <Typography variant="h6">Resources</Typography>
-            <ul>
-              <li>
-                <Typography variant="body1">
-                  Coping Strategies: Learn effective coping strategies.
-                </Typography>
-              </li>
-              <li>
-                <Typography variant="body1">
-                  CBT: Discover Cognitive-Behavioral Therapy Techniques and Resources
-                </Typography>
-              </li>
-            </ul>
-          </Grid>
-
-          <Grid item xs={12}>
-            <Typography variant="h6">Tracking Tools</Typography>
-            <ul>
-              <li>
-                <Typography variant="body1">
-                  Mood Tracker: Track your mood and gain insights.
-                </Typography>
-              </li>
-              <li>
-                <Typography variant="body1">
-                  Goal Setting: Set and track your personal goals.
-                </Typography>
-              </li>
-              <li>
-                <Typography variant="body1">
-                  Self Assessment: Evaluate your mental health status.
-                </Typography>
-              </li>
-              <li>
-                <Typography variant="body1">
-                  Habit Tracker: Monitor and build healthy habits.
-                </Typography>
-              </li>
-            </ul>
-          </Grid>
-
-          <Grid item xs={12}>
-            <Typography variant="h6">Meditation</Typography>
-            <ul>
-              <li>
-                <Typography variant="body1">
-                  Guided Meditation: Relax and find inner peace.
-                </Typography>
-              </li>
-            </ul>
-          </Grid>
-
-          <Grid item xs={12}>
-            <Typography variant="h6">Account</Typography>
-            <ul>
-              <li>
-                <Typography variant="body1">
-                  Login / Logout: Access or sign out from your account.
-                </Typography>
-              </li>
-              <li>
-                <Typography variant="body1">
-                  Survey: Provide valuable feedback
+        {slideImages.map((slide, index) => (
+          <Box
+            key={index}
+            data-tag={slide.tag}
+            ref={(el) => (slideRefs.current[index] = el)}
+            sx={{ position: "relative", height: "100vh", overflow: "hidden" }}
+          >
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: currentSlide - 1 === index ? 1 : 0 }}
+              transition={{ duration: 0.5 }}
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                backgroundImage: `url(${slide.imageUrl})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+            />
+            <Box sx={{ position: "absolute", top: "50%", left: 0, width: "100%", padding: 2, transform: "translateY(-50%)" }}>
+              <Card sx={{ backgroundColor: "rgba(255,255,255,0.75)" }}>
+                <CardContent>
+                  <Typography variant="h6" align="center">
+                    {slide.title}
                   </Typography>
-          </li>
-        </ul>
-      </Grid>
-    </Grid>
-  </Container>
-</Box>
+                  <Typography variant="body1" sx={{ marginTop: 1 }} align="center">
+                    {slide.description}
+                  </Typography>
 
+                  {slide.items && (
+                    <ul>
+                      {slide.items.map((item, idx) => (
+                        <li key={idx}>
+                          {slide.links && slide.links[idx] ? (
+                            <Link to={slide.links[idx]}>
+                            <Typography variant="body1" align="center">
+                              {item}
+                              </Typography>
+                            </Link>
+                          ) : (
+                            <Typography variant="body1" align="center">
+                              {item}
+                            </Typography>
+                          )}
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </CardContent>
+              </Card>
+            </Box>
+          </Box>
+        ))}
+      </Box>
+    </Box>
   );
 };
 
