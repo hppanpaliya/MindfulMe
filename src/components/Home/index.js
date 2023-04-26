9aca5d0a25c1ae2560010fe08ebc99e7dee7d2e9
hppanpaliya <hppanpaliya@gmail.com>
2023-04-26 04:09:38 -0400

    new homepage content

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -21,19 +21,13 @@ const Home = () => {
     <Box
       sx={{
         minHeight: "calc(100vh - 128px)",
-        background:
-          "linear-gradient(to bottom, #009688, #55c2c2 10%, #BBBBBB 100%)",
+        background: "linear-gradient(to bottom, #009688, #55c2c2 10%, #BBBBBB 100%)",
         color: "#ffffff",
         padding: 4,
       }}
     >
-      <Container maxWidth="md">
-        <motion.div
-          initial="hidden"
-          animate="visible"
-          variants={cardVariants}
-          transition={{ duration: 1 }}
-        >
+      <Container maxWidth="lg">
+        <motion.div initial="hidden" animate="visible" variants={cardVariants} transition={{ duration: 1 }}>
           <Typography variant="h4" align="center" gutterBottom>
             Welcome to our Mental Health Support Web App!
           </Typography>
@@ -43,13 +37,8 @@ const Home = () => {
         </motion.div>
         <Grid container spacing={4}>
           {slideImages.map((slide, index) => (
-            <Grid item xs={12} sm={6} md={4} key={index}>
-              <motion.div
-                initial="hidden"
-                animate="visible"
-                variants={cardVariants}
-                transition={{ duration: 0.5, delay: index * 0.2 }}
-              >
+            <Grid item xs={12} sm={6} md={3} key={index}>
+              <motion.div initial="hidden" animate="visible" variants={cardVariants} transition={{ duration: 0.5, delay: index * 0.2 }}>
                 <Card
                   sx={{
                     height: "100%",
@@ -62,11 +51,7 @@ const Home = () => {
                     <Typography variant="h6" align="center">
                       {slide.title}
                     </Typography>
-                    <Typography
-                      variant="body1"
-                      sx={{ marginTop: 1 }}
-                      align="center"
-                    >
+                    <Typography variant="body1" sx={{ marginTop: 1 }} align="center">
                       {slide.description}
                     </Typography>
                     {slide.items && (
