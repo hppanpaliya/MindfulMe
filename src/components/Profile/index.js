b44254b14dbab461f805b9a1eef55a1f23eadff2
hppanpaliya <hppanpaliya@gmail.com>
2023-04-29 01:31:49 -0400

    user profile settings page

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,12 +1,28 @@
 import React from "react";
 import DarkModeSwitch from "./settings/DarkModeSwitch";
+import styled from "@emotion/styled";
+import Typography from "@mui/material/Typography";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  max-width: 400px;
+  margin: auto;
+`;
 
 const Profile = () => {
   return (
-    <div>
-      <h1>Profile</h1>
-      <DarkModeSwitch />
-    </div>
+    <Container>
+      <div>
+        <Typography variant="h2" gutterBottom>
+          Profile
+        </Typography>
+        <DarkModeSwitch />
+      </div>
+    </Container>
   );
 };
 
